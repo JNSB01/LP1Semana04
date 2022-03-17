7a99c46a555e8f8a205f8f276c49312ea0537f94
João Borges <joaonunos.borges@gmail.com>
2022-03-17 20:09:13 +0000

    Exercicio4 added function CountoN

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,12 +6,27 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
+            string aux = Console.ReadLine();
+            int nCount = Convert.ToInt32(aux);
+            string aux2 = Console.ReadLine();
+            int nCount2 = Convert.ToInt32(aux);
+
             CountTo10();
             CountTo10();
+            CountToN(nCount);
+            CountToN(nCount2);
         }
         private static void CountTo10()
         {
             for (int i = 1; i <= 10; i++)
+            {
+                CountToN(10);
+            }
+        }
+
+        private static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine(i);
             }
