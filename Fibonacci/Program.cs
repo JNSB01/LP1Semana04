44881b753c951f6e95ae621c25c189aa0d43b99f
João Borges <joaonunos.borges@gmail.com>
2022-03-17 21:16:37 +0000

    Exercicio7 added prjoct Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Result = Fibonacci(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(Result);
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 2 | n == 1)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n-1) + Fibonacci(n-2);
+        }
+    }
+}
